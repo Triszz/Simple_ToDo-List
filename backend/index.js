576108e663d0ad4ab68fc97a7750c172e3b9795a
Triszz <tranthanhtri23022005@gmail.com>
2025-09-11 17:46:19 +0700

    update url for frontend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://playful-paprenjak-afda2c.netlify.app",
+      "https://tris-simple-todo-list.netlify.app",
     ],
     credentials: true,
   })
